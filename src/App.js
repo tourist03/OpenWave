5e56ed1bddf0cec80c22a4dc8263efa3622f2404
Vineet Singh <singhvineet2001@gmail.com>
2024-08-28 17:30:10 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React, { Component } from 'react';
 import Navbar from './component/Navbar';
-import News from './component/News'; // This should match the export from News.js
+import News from './component/News'; 
 
 export default class App extends Component {
   render() {
