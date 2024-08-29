7c4a4ae426154aab30bad79db7828d17e2e40c12
Vineet Singh <singhvineet2001@gmail.com>
2024-08-29 16:33:51 +0530

    Added loading function

--- src/component/News.js
+++ src/component/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import LoadingSpinner from "./LoadingSpinner";
 
 export class News extends Component {
   constructor() {
@@ -9,6 +10,7 @@ export class News extends Component {
       loading: false,
       page: 1,
       totalResults: 0,
+      hasMoreArticles: true,
     };
   }
 
@@ -18,10 +20,10 @@ export class News extends Component {
 
   fetchArticles = async () => {
     try {
-      const url = `https://newsapi.org/v2/everything?q=tesla&from=2024-07-28&sortBy=publishedAt&apiKey=d8f429a783e54d9daae50adde8627f2e&page=${this.state.page}&pageSize=20`;
+      const url = `https://newsapi.org/v2/everything?q=tesla&from=2024-07-29&sortBy=publishedAt&apiKey=670b2f702b4444e2a5e2d1cc01ef8722&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+      this.setState({loading : true})
       const data = await fetch(url);
       const parseData = await data.json();
-      //this.setState({ articles: parseData.articles });
       if (parseData.articles && Array.isArray(parseData.articles)) {
         const uniqueArticles = parseData.articles.filter(
           (article, index, self) =>
@@ -30,45 +32,28 @@ export class News extends Component {
 
         this.setState({
           articles: uniqueArticles,
-          totalResults: parseData.totalResults || 0,
+          totalResults: parseData.totalResults,
+          hasMoreArticles: uniqueArticles.length > 0,
+          loading: false,
         });
       } else {
-        // Handle unexpected data structure
         console.error("Unexpected data structure:", parseData);
-        this.setState({ articles: [], totalResults: 0 });
+        this.setState({ articles: [], totalResults: 0 , hasMoreArticles: false, loading: false });
       }
     } catch (error) {
       console.error("Error fetching articles:", error);
-      this.setState({ articles: [], totalResults: 0 });
+      this.setState({ articles: [], totalResults: 0 , hasMoreArticles: false, loading: false});
     }
   };
 
   handleNextClick = async () => {
     console.log("next");
-    if (this.state.page + 1 <= Math.ceil(this.state.totalResults / 20)) {
+    if (this.state.page + 1 <= Math.ceil(this.state.totalResults /this.props.pageSize)) {
       this.setState(
         (prevState) => ({ page: prevState.page + 1 }),
         this.fetchArticles
       );
     }
-
-    // } else {
-    //   let url = `https://newsapi.org/v2/everything?q=tesla&from=2024-07-28&sortBy=publishedAt&apiKey=d8f429a783e54d9daae50adde8627f2e&page=${
-    //     this.state.page + 1
-    //   }&pageSize=20`;
-    //   let data = await fetch(url);
-    //   let parseData = await data.json();
-    //   this.setState({ articles: parseData.articles });
-    //   const uniqueArticles = parseData.articles.filter(
-    //     (article, index, self) =>
-    //       index === self.findIndex((t) => t.url === article.url)
-    //   );
-
-    //   this.setState({
-    //     page: this.state.page + 1,
-    //     articles: uniqueArticles,
-    //   });
-    // }
   };
 
   handlePrevClick = async () => {
@@ -79,29 +64,15 @@ export class News extends Component {
         this.fetchArticles
       );
     }
-    // let url = `https://newsapi.org/v2/everything?q=tesla&from=2024-07-28&sortBy=publishedAt&apiKey=d8f429a783e54d9daae50adde8627f2e&page=${
-    //   this.state.page - 1
-    // }&pageSize=20`;
-    // let data = await fetch(url);
-    // let parseData = await data.json();
-    // this.setState({ articles: parseData.articles });
-    // const uniqueArticles = parseData.articles.filter(
-    //   (article, index, self) =>
-    //     index === self.findIndex((t) => t.url === article.url)
-    // );
-
-    // this.setState({
-    //   page: this.state.page - 1,
-    //   articles: uniqueArticles,
-    // });
   };
 
   render() {
     return (
       <div className="container my-4">
-        <h2>OpenWave - Top Headline</h2>
+        <h1 className="text-center">OpenWave - Top Headline</h1>
+        {this.state.loading && <LoadingSpinner/>}
         <div className="row">
-          {this.state.articles.map((element) => {
+          {!this.state.loading && this.state.articles.map((element) => {
             return (
               <div className="col-md-4" key={element.url}>
                 <NewsItem
@@ -124,6 +95,7 @@ export class News extends Component {
             &larr; Previous
           </button>
           <button
+            disabled = {!this.state.hasMoreArticles}
             type="button"
             className="btn btn-outline-success"
             onClick={this.handleNextClick}
